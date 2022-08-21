c3ba3e4c7e06ebd8f363e5742ea54c02c53f12ae
Facundo Esquivel <f4cu.3s@gmail.com>
2022-08-21 18:58:17 -0300

    Ajustes de formato

--- js/init.js
+++ js/init.js
@@ -7,83 +7,83 @@ const CART_INFO_URL = "https://japceibal.github.io/emercado-api/user_cart/";
 const CART_BUY_URL = "https://japceibal.github.io/emercado-api/cart/buy.json";
 const EXT_TYPE = ".json";
 
-let showSpinner = function(){
+let showSpinner = function () {
   document.getElementById("spinner-wrapper").style.display = "block";
 }
 
-let hideSpinner = function(){
+let hideSpinner = function () {
   document.getElementById("spinner-wrapper").style.display = "none";
 }
 
-let getJSONData = function(url){
-    let result = {};
-    showSpinner();
-    return fetch(url)
+let getJSONData = function (url) {
+  let result = {};
+  showSpinner();
+  return fetch(url)
     .then(response => {
       if (response.ok) {
         return response.json();
-      }else{
+      } else {
         throw Error(response.statusText);
       }
     })
-    .then(function(response) {
-          result.status = 'ok';
-          result.data = response;
-          hideSpinner();
-          return result;
+    .then(function (response) {
+      result.status = 'ok';
+      result.data = response;
+      hideSpinner();
+      return result;
     })
-    .catch(function(error) {
-        result.status = 'error';
-        result.data = error;
-        hideSpinner();
-        return result;
+    .catch(function (error) {
+      result.status = 'error';
+      result.data = error;
+      hideSpinner();
+      return result;
     });
 }
 
 // Gestion de menus de usuario
 let botonCuentaUsuario = document.getElementById("cuenta-usuario");
-  let botonCrearUsuario = document.getElementById("crear-cuenta");
-  let menuOpcionesUsuario = document.getElementById("menu-usuario");
-  var usuarioActual = null;
+let botonCrearUsuario = document.getElementById("crear-cuenta");
+let menuOpcionesUsuario = document.getElementById("menu-usuario");
+var usuarioActual = null;
 
-  function adquiereNombreUsuario() {
-    let mantenerSesionIniciada = localStorage.getItem("mantenersesioniniciada");
+function adquiereNombreUsuario() {
+  let mantenerSesionIniciada = localStorage.getItem("mantenersesioniniciada");
 
-    if (mantenerSesionIniciada) {
-      usuarioActual = JSON.parse(localStorage.getItem("usuario"));
-    } else {
-      usuarioActual = JSON.parse(sessionStorage.getItem("usuario"));
-    }
+  if (mantenerSesionIniciada) {
+    usuarioActual = JSON.parse(localStorage.getItem("usuario"));
+  } else {
+    usuarioActual = JSON.parse(sessionStorage.getItem("usuario"));
   }
+}
 
-  function cerrarSesion() {
-    sessionStorage.removeItem('usuario');
-    localStorage.removeItem('usuario');
-    localStorage.removeItem('mantenersesioniniciada');
-    window.location.href = "index.html";
+function cerrarSesion() {
+  sessionStorage.removeItem('usuario');
+  localStorage.removeItem('usuario');
+  localStorage.removeItem('mantenersesioniniciada');
+  window.location.href = "index.html";
+}
+
+window.onload = function () {
+  adquiereNombreUsuario()
+  if (usuarioActual == null) {
+    // Cuando no hay usuario logeado se oculta el menu de usuario y se muestra el boton de crear cuenta y el de iniciar sesión
+    botonCuentaUsuario.innerText = "Iniciar sesión";
+    botonCuentaUsuario.classList.replace("nav-link", "btn", "active");
+    botonCuentaUsuario.classList.add("btn-success");
+    botonCuentaUsuario.href = "login.html";
+
+    menuOpcionesUsuario.style = "display: none";
+
+    // Por requisito de la letra se fuerza el inicio de sesion
+    window.location = "login.html"
+  } else {
+    botonCrearUsuario.style = "display: none";
+
+    botonCuentaUsuario.innerText = usuarioActual.nombre_completo;
+    botonCuentaUsuario.classList.replace("nav-link", "btn", "active");
+    botonCuentaUsuario.classList.add("btn-primary");
+    botonCuentaUsuario.href = "#";
+
+    menuOpcionesUsuario.style = "display: block";
   }
-
-  window.onload = function () {
-    adquiereNombreUsuario()
-    if (usuarioActual == null) {
-      // Cuando no hay usuario logeado se oculta el menu de usuario y se muestra el boton de crear cuenta y el de iniciar sesión
-      botonCuentaUsuario.innerText = "Iniciar sesión";
-      botonCuentaUsuario.classList.replace("nav-link", "btn", "active");
-      botonCuentaUsuario.classList.add("btn-success");
-      botonCuentaUsuario.href = "login.html";
-
-      menuOpcionesUsuario.style = "display: none";
-
-      // Por requisito de la letra se fuerza el inicio de sesion
-      window.location = "login.html"
-    } else {
-      botonCrearUsuario.style = "display: none";
-      
-      botonCuentaUsuario.innerText = usuarioActual.nombre_completo;
-      botonCuentaUsuario.classList.replace("nav-link", "btn", "active");
-      botonCuentaUsuario.classList.add("btn-primary");
-      botonCuentaUsuario.href = "#";
-
-      menuOpcionesUsuario.style = "display: block";
-    }
-  }
+}
--- js/products.js
+++ js/products.js
@@ -8,28 +8,27 @@ let maxCount = undefined;
 
 // Ordena los elementos del array recibido
 // Cuando se define criterio la funcion de comparacion adecuada es utilizada
-function sortProducts(criteria, array){
+function sortProducts(criteria, array) {
     let result = [];
-    if (criteria === ORDER_ASC_BY_NAME)
-    {
-        result = array.sort(function(a, b) {
-            if ( a.name < b.name ){ return -1; }
-            if ( a.name > b.name ){ return 1; }
+    if (criteria === ORDER_ASC_BY_NAME) {
+        result = array.sort(function (a, b) {
+            if (a.name < b.name) { return -1; }
+            if (a.name > b.name) { return 1; }
             return 0;
         });
-    }else if (criteria === ORDER_DESC_BY_NAME){
-        result = array.sort(function(a, b) {
-            if ( a.name > b.name ){ return -1; }
-            if ( a.name < b.name ){ return 1; }
+    } else if (criteria === ORDER_DESC_BY_NAME) {
+        result = array.sort(function (a, b) {
+            if (a.name > b.name) { return -1; }
+            if (a.name < b.name) { return 1; }
             return 0;
         });
-    }else if (criteria === ORDER_BY_PRICE){
-        result = array.sort(function(a, b) {
+    } else if (criteria === ORDER_BY_PRICE) {
+        result = array.sort(function (a, b) {
             let aCost = parseInt(a.cost);
             let bCost = parseInt(b.cost);
 
-            if ( aCost > bCost ){ return -1; }
-            if ( aCost < bCost ){ return 1; }
+            if (aCost > bCost) { return -1; }
+            if (aCost < bCost) { return 1; }
             return 0;
         });
     }
@@ -39,19 +38,19 @@ function sortProducts(criteria, array){
 
 // Guarda el valor del producto elegido en el Almacen Local del navegador accesible por la página "product-info.html"
 function setProdID(id) {
-   localStorage.setItem("prodID", id);
-   window.location = "product-info.html"
+    localStorage.setItem("prodID", id);
+    window.location = "product-info.html"
 }
 
 // Crea el contenido HTML que muestra los productos
-function showProductsList(){
+function showProductsList() {
 
     let htmlContentToAppend = "";
-    for(let i = 0; i < currentProductsArray.length; i++){
+    for (let i = 0; i < currentProductsArray.length; i++) {
         let product = currentProductsArray[i];
 
         if (((minCount == undefined) || (minCount != undefined && parseInt(product.cost) >= minCount)) &&
-            ((maxCount == undefined) || (maxCount != undefined && parseInt(product.cost) <= maxCount))){
+            ((maxCount == undefined) || (maxCount != undefined && parseInt(product.cost) <= maxCount))) {
 
             htmlContentToAppend += `
             <div onclick="setProdID(${product.id})" class="list-group-item list-group-item-action cursor-active">
@@ -76,10 +75,10 @@ function showProductsList(){
 }
 
 // Ordena y muestra los productos
-function sortAndShowProducts(sortCriteria, productsArray){
+function sortAndShowProducts(sortCriteria, productsArray) {
     currentSortCriteria = sortCriteria;
 
-    if(productsArray != undefined){
+    if (productsArray != undefined) {
         currentProductsArray = productsArray;
     }
 
@@ -92,33 +91,33 @@ function sortAndShowProducts(sortCriteria, productsArray){
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
-    getJSONData(PRODUCTS_URL + localStorage.getItem("catID") + ".json").then(function(resultObj){
-        if (resultObj.status === "ok"){
+document.addEventListener("DOMContentLoaded", function (e) {
+    getJSONData(PRODUCTS_URL + localStorage.getItem("catID") + ".json").then(function (resultObj) {
+        if (resultObj.status === "ok") {
             currentProductsArray = resultObj.data.products
             document.getElementById('titulo-productos').innerHTML = "Verás aquí todos los productos de la categoría " + resultObj.data.catName;
             showProductsList()
             //sortAndShowProducts(ORDER_ASC_BY_NAME, resultObj.data);
         }
-        
+
     });
 
     // Al llamar a esta funcion no se le pasa el parámetro productsArray ya que
     // este fue inicializado con la escucha del evento "DOMContentLoaded"
-    document.getElementById("sortAsc").addEventListener("click", function(){
+    document.getElementById("sortAsc").addEventListener("click", function () {
         sortAndShowProducts(ORDER_ASC_BY_NAME);
     });
 
-    document.getElementById("sortDesc").addEventListener("click", function(){
+    document.getElementById("sortDesc").addEventListener("click", function () {
         sortAndShowProducts(ORDER_DESC_BY_NAME);
     });
 
-    document.getElementById("sortByCount").addEventListener("click", function(){
+    document.getElementById("sortByCount").addEventListener("click", function () {
         sortAndShowProducts(ORDER_BY_PRICE);
     });
 
     // Vacía los valores establecidos en el filtro de rango de cantidad de productos
-    document.getElementById("clearRangeFilter").addEventListener("click", function(){
+    document.getElementById("clearRangeFilter").addEventListener("click", function () {
         document.getElementById("rangeFilterCountMin").value = "";
         document.getElementById("rangeFilterCountMax").value = "";
 
@@ -128,22 +127,22 @@ document.addEventListener("DOMContentLoaded", function(e){
         showProductsList();
     });
 
-    document.getElementById("rangeFilterCount").addEventListener("click", function(){
+    document.getElementById("rangeFilterCount").addEventListener("click", function () {
         //Obtengo el mínimo y máximo de los intervalos para filtrar por precio de productos
         minCount = document.getElementById("rangeFilterCountMin").value;
         maxCount = document.getElementById("rangeFilterCountMax").value;
 
-        if ((minCount != undefined) && (minCount != "") && (parseInt(minCount)) >= 0){
+        if ((minCount != undefined) && (minCount != "") && (parseInt(minCount)) >= 0) {
             minCount = parseInt(minCount);
         }
-        else{
+        else {
             minCount = undefined;
         }
 
-        if ((maxCount != undefined) && (maxCount != "") && (parseInt(maxCount)) >= 0){
+        if ((maxCount != undefined) && (maxCount != "") && (parseInt(maxCount)) >= 0) {
             maxCount = parseInt(maxCount);
         }
-        else{
+        else {
             maxCount = undefined;
         }
 
